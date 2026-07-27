cf94abbcf33023896e34ea24f6c9e5dfb4901b06
jainpavni660-sketch <jainpavni660@gmail.com>
2026-07-27 13:53:57 +0530

    Change developer name in footer to Pavni Jain
    Updated developer information in the footer section.

--- app.py
+++ app.py
@@ -146,10 +146,7 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="teal", neutral_hue="slate")) as
     gr.Markdown("""
     ---
     ### 👨‍💻 About the Developer
-    **Created by:** Chandan Saroj (MERN Stack Developer & SDE)
-    * **LinkedIn:** [Connect with me](YOUR_LINKEDIN_URL_HERE)
-    * **GitHub:** [Check out my projects](YOUR_GITHUB_URL_HERE)
-    """)
+    **Created by:** Pavni Jain
 
     # Wire up the logic exactly in the order of the dataset
     input_components = [
